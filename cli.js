dec371530f885af4424e1b8bcbf8ca3948f0fe8e
Mo Binni <mo.binni@rangle.io>
2018-04-01 23:51:49 -0400

    added cli tool and rewrote to allow nested keys
    added cli tool and rewrote to allow nested keys
    
    Added working cli
    
    Added working cli
    
    version bump
    
    fixed tests
    
    version bump
    
    remove build folder from git
    
    added circleci integration
    
    added circleci integration
    
    added workflows
    
    create env file on build
    
    adjust creation
    
    create env file on build
    
    create env file on build

---
+++ cli.js
@@ -0,0 +1,31 @@
+const program = require("commander");
+const path = require("path");
+const fs = require("fs");
+import translate from "./src/translate";
+
+program
+    .arguments("<file>")
+    .option("-k, --key <key>", "Yandex API key")
+    .option("-i, --iso <code>", "isoCode to translate to")
+    .option("-r, --regexp <expression>", "regular expression to filter on")
+    .action(function(file) {
+        const { key: apiKey, iso: isoCode, regexp = "//g" } = program;
+        if (!apiKey) throw new Error("No Yandex API key supplied");
+        if (!isoCode) throw new Error("No iso code to translate to supplied");
+        const filePath = path.normalize(file);
+        fs.readFile(filePath, "utf8", async function(err, data) {
+            if (err) throw err;
+            const translations = JSON.parse(data);
+            const result = await translate({
+                apiKey,
+                isoCode,
+                translations,
+                regexp
+            });
+            fs.writeFile(
+                path.dirname(filePath) + path.join(`/${isoCode}.json`),
+                JSON.stringify(result)
+            );
+        });
+    })
+    .parse(process.argv);
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,74 @@
-export const extractKeys = obj => Object.keys(obj);
-export const compose = (...fs) => fs.reduce((f, g) => (...xs) => f(g(...xs)));
-export const pipe = (...fs) => fs.reduceRight((f, g) => (...xs) => f(g(...xs)));
-export const curry = (f, ...xs) =>
-    xs.length >= f.length ? f(...xs) : (...other) => curry(f, ...xs, ...other);
-export const map = fn => x => x.map(fn);
-export const flatten = arr => arr.reduce((acc, e) => acc.concat(e), []);
-export const merge = arr => {
-    const obj = {};
-    arr.map(e => Object.assign(obj, e));
-    return [obj];
+import { keys } from "ramda";
+
+import { isoCodes } from "./iso-codes";
+
+export const verifyISOCode = code => !!isoCodes.find(iso => iso.code === code);
+
+export const traverse = obj => {
+    const verify = obj => typeof obj === "string" || typeof obj === "number";
+    const results = [];
+    function loopKeys(obj, prevPath) {
+        const objKeys = keys(obj);
+        objKeys.forEach(k => {
+            let path = [];
+            if (prevPath) path = prevPath;
+            if (!verify(obj[k])) {
+                path.push(k);
+                return loopKeys(obj[k], path);
+            }
+
+            results.push({
+                path: [].concat(path, k),
+                phrase: obj[k]
+            });
+        });
+    }
+    loopKeys(obj);
+    return results;
 };
+
+export const revertInterpolations = placeholder => ({
+    interpolations,
+    phrase
+}) => {
+    let originalPhrase = new String(phrase);
+    if (!interpolations || !interpolations.length) return phrase;
+    interpolations.map(({ value }) => {
+        originalPhrase = originalPhrase.replace(placeholder, () => value);
+    });
+    return originalPhrase;
+};
+
+export const replaceInterpolations = placeholder => ({
+    interpolations,
+    phrase,
+    ...other
+}) => {
+    let newPhrase = new String(phrase);
+    interpolations.map(({ value }) => {
+        newPhrase = newPhrase.replace(value, () => placeholder);
+    });
+    return { phrase: newPhrase.toString(), interpolations, ...other };
+};
+
+export const findInterpolations = regexp => ({ phrase, ...other }) => {
+    const regex = new RegExp(regexp.toString(), "g");
+    const interpolations = [];
+    let match;
+    while ((match = regex.exec(phrase))) {
+        if (match) {
+            interpolations.push({ value: match[0], index: regex.lastIndex });
+        }
+    }
+    return {
+        interpolations,
+        phrase,
+        ...other
+    };
+};
+
+export const buildEndpoint = (apiKey, isoCode, phrase) =>
+    "https://translate.yandex.net/api/v1.5/tr.json/translate?" +
+    `lang=${isoCode}` +
+    `&key=${apiKey}` +
+    `&text=${encodeURIComponent(phrase)}`;
