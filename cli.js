4bd7d1eff3fefa998c208839e75a8a20d126cc54
Mo Binni <mo.binni@rangle.io>
2018-04-12 01:10:50 -0400

    update cli help file

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ const translate = require("./cli/translate").default;
 
 program
     .arguments("<file>")
-    .option("-s, --service [google, yandex = default]", "Yandex API key")
+    .option("-s, --service [google, yandex (default), bing]", "Yandex API key")
     .option("-k, --key <key>", "API key")
     .option("-i, --iso <code>", "isoCode to translate to")
     .option("-r, --regexp <expression>", "regular expression to filter on")
