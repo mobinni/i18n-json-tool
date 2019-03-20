8a157ca137a12aef4613538944fa7bc5bf90d597
Mo Binni <mo.binni@rangle.io>
2019-03-20 15:39:11 +0100

    fix tests

--- src/translate.test.js
+++ src/translate.test.js
@@ -55,7 +55,9 @@ describe("Translation test suite", () => {
             expect(results).toEqual({
                 deeply: {
                     nested: {
-                        key: { letters: "aantal", number: "1", sequence: "seq" }
+                        key: { sequence: "seq" },
+                        letters: "aantal",
+                        number: "1"
                     }
                 },
                 key: "Wereld",
